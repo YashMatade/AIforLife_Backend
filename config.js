59c3d803ae88352e9c94cd29f8192343c98eb745
yashmatade23 <yash.sandip@primatesdev.com>
2024-03-02 11:26:46 +0530

    uri

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.mongodbConnection = () => {
-    const mongoURL = process.env.MONGOURL;
+    const mongoURL = "mongodb+srv://anm:XBVpcdyavDv5Jm6X@cluster0.alhfdqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     mongoose.connect(mongoURL).then(() => {
         console.log("Connected to database");
     }).catch((error) => {
